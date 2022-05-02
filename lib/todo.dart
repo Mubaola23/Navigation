4bf9b4e1af5d47461b566148d10770d1a350726e
MubaH_dev <mubarakshuaib3@gmail.com>
2022-05-02 20:52:17 +0100

    update

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TodoApp extends StatefulWidget {
@@ -8,9 +9,43 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
+  //todo list items
+  final List _todoItems = [];
+
+  // controller to help us track the changes in our text field
+  final textController = TextEditingController();
+
+  // Function to add todo items
+
+  addTodo(String task) {
+    if (task.isNotEmpty) {
+      setState(() {
+        _todoItems.add(task);
+      });
+    }
+  }
+
+  deleteTodo(int index) {
+    setState(() {
+      _todoItems.removeAt(index);
+    });
+  }
+  // onDispose() {
+  //   // clears the text field
+  //   textController.dispose();
+  //
+  //   super.dispose();
+  // }
+  // onInit(){
+  //
+  //   // Perform operation on start of a screen
+  //   super.initState();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+// floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orange,
         onPressed: () {
@@ -20,8 +55,13 @@ class _TodoAppState extends State<TodoApp> {
                     padding: EdgeInsets.all(24),
                     child: Column(
                       children: [
-                        const TextField(
-                          decoration: InputDecoration(
+                        TextField(
+                          controller: textController,
+                          onChanged: (value) {
+                            print(value);
+                            print(textController.text);
+                          },
+                          decoration: const InputDecoration(
                               label: Text("Enter your todo here"),
                               labelStyle: TextStyle(
                                   color: Colors.orange, fontSize: 18)),
@@ -30,14 +70,18 @@ class _TodoAppState extends State<TodoApp> {
                           height: 16,
                         ),
                         RaisedButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            addTodo(textController.text);
+                            textController.clear();
+                            Navigator.pop(context);
+                          },
                           child: Text("Add Todo"),
                         )
                       ],
                     ),
                   ));
         },
-        child: Icon(Icons.add, color: Colors.white),
+        child: const Icon(Icons.add, color: Colors.white),
       ),
       appBar: AppBar(
         backgroundColor: Colors.orange,
@@ -50,26 +94,67 @@ class _TodoAppState extends State<TodoApp> {
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.all(24),
-        child: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (context, index) => Card(
-            child: ListTile(
-              onTap: () {
-                showModalBottomSheet(
-                    context: context,
-                    builder: (context) => Container(
-                        height: 100,
-                        padding: EdgeInsets.all(24),
-                        child: Text("Herre is your to do")));
-              },
-              title: Text("Your Text here"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            const FlutterLogo(
+              size: 30,
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemCount: _todoItems.length,
+                itemBuilder: (context, index) => Card(
+                  child: ListTile(
+                    title: GestureDetector(
+                        onTap: () {
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (context) => Container(
+                              padding: const EdgeInsets.all(24),
+                              child: Text(
+                                _todoItems[index],
+                              ),
+                            ),
+                          );
+                        },
+                        child: Text(_todoItems[index])),
+                    trailing: IconButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) => Container(
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 24, vertical: 50),
+                                  color: Colors.white,
+                                  height:
+                                      MediaQuery.of(context).size.height / 3,
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      const Text(
+                                          "Are you Sure you want to delete?"),
+                                      RaisedButton(
+                                        onPressed: () {
+                                          deleteTodo(index);
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text("Delete Todo"),
+                                      ),
+                                    ],
+                                  ),
+                                ));
+                      },
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
